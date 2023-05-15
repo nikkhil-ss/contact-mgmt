1e09f71f1548c92e7704004abd91889eecb41fcd
nikkhil-ss <nikhil.singh_cs19@gla.ac.in>
2023-05-15 22:00:46 +0530

    favico

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  return <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
-        <Link to="/" className='navbar-brand ml-10'>Contact App</Link>
+  return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2" >
+        <Link to="/" className='navbar-brand ' style={{marginLeft: "20px"}} >Contact App</Link>
   </nav>
-  
+  )
 };
 
 export default NavBar;
